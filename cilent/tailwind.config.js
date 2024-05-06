999af0eb5062e1eed61d230d25576691fa0d45c2
adugbo aghogho <adugboaghogho@gmail.com>
2024-05-06 19:26:25 +0100

    mn

--- cilent/tailwind.config.js
+++ cilent/tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
+    "./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"
   ],
   mode: "jit",
   darkMode: false, // or 'media' or 'class'
