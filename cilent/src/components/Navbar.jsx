cf5759ab925f0a9d2d6b932d4be4a994087ab529
adugbo aghogho <adugboaghogho@gmail.com>
2024-05-06 18:09:41 +0100

    dss

--- cilent/src/components/Navbar.jsx
+++ cilent/src/components/Navbar.jsx
@@ -5,7 +5,7 @@ import { AiOutlineClose } from "react-icons/ai";
 import logo from "../../images/logo.png";
 
 const NavBarItem = ({ title, classprops }) => (
-  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+  <li className={`mx-4 cursor-pointer hover:text-blue-200 ${classprops}`}>{title}</li>
 );
 
 const Navbar = () => {
@@ -40,6 +40,9 @@ const Navbar = () => {
             {["Market", "Exchange", "Tutorials", "Wallets"].map(
               (item, index) => <NavBarItem key={item + index} title={item} classprops="my-2 text-lg" />,
             )}
+            <li className="bg-[#2952e3] py-2 px-7 mx-4 rounded-full cursor-pointer hover:bg-[#2546bd]">
+              Login
+            </li>
           </ul>
         )}
       </div>
