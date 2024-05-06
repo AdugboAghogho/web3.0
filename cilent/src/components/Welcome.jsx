c0460f37bbbca1d22d8b5644c759f842d8759c09
adugbo aghogho <adugboaghogho@gmail.com>
2024-05-06 19:04:15 +0100

    m,n

--- cilent/src/components/Welcome.jsx
+++ cilent/src/components/Welcome.jsx
@@ -42,13 +42,15 @@ const Welcome = () => {
   return (
     <div className="flex w-full justify-center items-center">
       <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-        <div className="flex flex-1 justify-start items-start flex-col mf:mr-10">
+        <div className="flex flex-1 justify-start items-start flex-col md:mr-10">
           <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">
             Send Crypto <br /> across the world
           </h1>
+
           <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">
             Explore the crypto world. Buy and sell cryptocurrencies easily on Krypto.
           </p>
+
           {/* {!currentAccount && ( */}
             <button
               type="button"
@@ -80,19 +82,21 @@ const Welcome = () => {
           </div>
         </div>
 
-        <div className="flex flex-col flex-1 items-center justify-start w-full mf:mt-0 mt-10">
+        <div className="flex flex-col flex-1 items-center justify-start w-full md:mt-0 mt-10">
           <div className="p-3 flex justify-end items-start flex-col rounded-xl h-40 sm:w-72 w-full my-5 eth-card .white-glassmorphism ">
             <div className="flex justify-between flex-col w-full h-full">
               <div className="flex justify-between items-start">
                 <div className="w-10 h-10 rounded-full border-2 border-white flex justify-center items-center">
                   <SiEthereum fontSize={21} color="#fff" />
                 </div>
+
                 <BsInfoCircle fontSize={17} color="#fff" />
               </div>
+              
               <div>
                 <p className="text-white font-light text-sm">
                   {/* {shortenAddress(currentAccount)} */}
-                  p
+                    Address
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
@@ -100,6 +104,8 @@ const Welcome = () => {
               </div>
             </div>
           </div>
+
+
           <div className="p-5 sm:w-96 w-full flex flex-col justify-start items-center blue-glassmorphism">
             <Input placeholder="Address To" name="addressTo" type="text" handleChange={handleChange} />
             <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
@@ -108,7 +114,8 @@ const Welcome = () => {
 
             <div className="h-[1px] w-full bg-gray-400 my-2" />
 
-            {isLoading
+            {/* {isLoading */}
+            {false
               ? <Loader />
               : (
                 <button
