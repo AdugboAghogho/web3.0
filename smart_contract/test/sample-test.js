df87c532df26689faee4cc1a39641c669e463a79
adugbo aghogho <adugboaghogho@gmail.com>
2024-05-08 12:10:28 +0100

    cxx

--- smart_contract/test/sample-test.js
+++ smart_contract/test/sample-test.js
@@ -1,19 +1,20 @@
-const { expect } = require("chai");
-const { ethers } = require("hardhat");
+const main = async () => {
+  const transactionsFactory = await hre.ethers.getContractFactory("Transactions");
+  const transactionsContract = await transactionsFactory.deploy();
 
-describe("Greeter", function () {
-  it("Should return the new greeting once it's changed", async function () {
-    const Greeter = await ethers.getContractFactory("Greeter");
-    const greeter = await Greeter.deploy("Hello, world!");
-    await greeter.deployed();
+  await transactionsContract.deployed();
 
-    expect(await greeter.greet()).to.equal("Hello, world!");
+  console.log("Transactions address: ", transactionsContract.address);
+};
 
-    const setGreetingTx = await greeter.setGreeting("Hola, mundo!");
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.error(error);
+    process.exit(1);
+  }
+};
 
-    // wait until the transaction is mined
-    await setGreetingTx.wait();
-
-    expect(await greeter.greet()).to.equal("Hola, mundo!");
-  });
-});
+runMain();
